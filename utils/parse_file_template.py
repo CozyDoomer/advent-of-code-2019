ba5153e485326501e97742eec7b0c1db1437b660
DollofCuty <chr.unterrainer@gmail.com>
2019-12-02 14:45:16 +0100

    add parser for comma seperated file

--- utils/parse_file_template.py
+++ utils/parse_file_template.py
@@ -1,4 +1,9 @@
-def parse_input_file(path, type):
+def parse_input_file_linewise(path, type):
     with open(path) as input_file:
         input_list = list(map(type, input_file.read().splitlines()))
+        return input_list
+
+def parse_input_file_comma_seperated(path, type):
+    with open(path) as input_file:
+        input_list = list(map(type, input_file.read().split(",")))
         return input_list
