cad655a7f53fa69a78ded5d5c3b3b9c85de86f23
DollofCuty <chr.unterrainer@gmail.com>
2019-12-08 23:54:54 +0100

    add parsing from file returning unsplitted string

--- utils/parse_file_template.py
+++ utils/parse_file_template.py
@@ -1,7 +1,14 @@
+def parse_input_file_string(path, type):
+    with open(path) as input_file:
+        input_list = input_file.read()
+        return input_list
+
+
 def parse_input_file_linewise(path, type):
     with open(path) as input_file:
         input_list = list(map(type, input_file.read().splitlines()))
         return input_list
+
 
 def parse_input_file_seperated(path, seperator, type):
     with open(path) as input_file:
